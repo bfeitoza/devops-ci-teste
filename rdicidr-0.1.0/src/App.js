0068420839b6374fa1894ed5d85710c2e2d1e296
bfeitoza <brunor.feitoza@gmail.com>
2022-06-04 21:16:13 -0300

    Update App.js

--- rdicidr-0.1.0/src/App.js
+++ rdicidr-0.1.0/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Calculatoor</div>
+        <div>CIDR Calculator</div>
       </header>
       <IPv4Addr />
       <footer>
